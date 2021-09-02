847f1b9f2b6eadc2f6bf554ad7e66deff53a33ac
Jonathas Tassi <jonathastassi@hotmail.com>
2021-09-02 10:21:48 -0300

    Created getIssLocation usecase and tests

---
+++ test/app/domain/usecases/get_iss_location_map_usecase_test.dart
@@ -0,0 +1,47 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:info_space_app/app/domain/entities/iss_location_map_entity.dart';
+import 'package:info_space_app/app/domain/entities/posititon_entity.dart';
+import 'package:info_space_app/app/domain/repositories/i_iss_location_map_repository.dart';
+import 'package:info_space_app/app/domain/usecases/get_iss_location_map_usecase.dart';
+import 'package:info_space_app/core/errors/failures.dart';
+import 'package:info_space_app/core/usecases/usecase.dart';
+import 'package:mocktail/mocktail.dart';
+
+class MockIssLocationMapRepository extends Mock
+    implements IIssLocationMapRepository {}
+
+void main() {
+  group('Domain - usecases - GetIssLocationMapUsecase', () {
+    late GetIssLocationMapUsecase usecase;
+    late IIssLocationMapRepository issLocationMapRepositoryMock;
+
+    setUp(() {
+      issLocationMapRepositoryMock = MockIssLocationMapRepository();
+      usecase = GetIssLocationMapUsecase(
+          issLocationMapRepository: issLocationMapRepositoryMock);
+    });
+
+    test('Should get last position of iss from repository', () async {
+      final mockIssLocation = IssLocationMapEntity(
+        position: PositionEntity(
+          latitude: '123',
+          longitude: '567',
+        ),
+        timestamp: '1234567',
+      );
+
+      when(() => issLocationMapRepositoryMock.getIssLocation()).thenAnswer(
+        (_) async => Right<Failure, IssLocationMapEntity>(
+          mockIssLocation,
+        ),
+      );
+
+      final result = await usecase.call(NoParams());
+
+      expect(result, Right<Failure, IssLocationMapEntity>(mockIssLocation));
+      verify(() => issLocationMapRepositoryMock.getIssLocation()).called(1);
+      verifyNoMoreInteractions(issLocationMapRepositoryMock);
+    });
+  });
+}
