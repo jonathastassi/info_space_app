5a3bc93d438065d0a9cead31a9741173ef9ec766
Jonathas Tassi <jonathastassi@hotmail.com>
2021-09-03 15:55:31 -0300

    adjust peoplesInSpacePage

--- lib/app/presentation/pages/peoples_in_space/peoples_in_space_page.dart
+++ lib/app/presentation/pages/peoples_in_space/peoples_in_space_page.dart
@@ -4,6 +4,7 @@ import 'package:info_space_app/app/presentation/pages/peoples_in_space/peoples_i
 import 'package:info_space_app/app/presentation/pages/peoples_in_space/peoples_in_space_state.dart';
 import 'package:info_space_app/app/presentation/utils/show_snackbar.dart';
 import 'package:info_space_app/app/presentation/widgets/datatable_custom.dart';
+import 'package:info_space_app/app/presentation/widgets/header_page.dart';
 import 'package:info_space_app/core/dependency_injection/dependency_injection_config.dart';
 
 class PeoplesInSpacePage extends StatefulWidget {
@@ -47,59 +48,29 @@ class _PeoplesInSpacePageState extends State<PeoplesInSpacePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              color: Theme.of(context).primaryColor,
-              padding: const EdgeInsets.all(16),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    'People in Space Right Now',
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).secondaryHeaderColor,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16,
-                  ),
-                  Text(
-                    'The number of people in space at this moment. List of names when known.',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: ValueListenableBuilder<PeoplesInSpaceState>(
-                builder: (_, state, __) {
-                  return DatatableCustom<PeopleInSpaceEntity>(
-                    data: state.peopleInSpaceList,
-                    loading: state.isLoading,
-                    onRefresh: _getData,
-                    itemHeader: [
-                      DataColumn(label: Text('Name')),
-                      DataColumn(label: Text('Craft'))
-                    ],
-                    itemRow: (people) => [
-                      DataCell(Text(people.name)),
-                      DataCell(Text(people.craft), placeholder: true),
-                    ],
-                  );
-                },
-                valueListenable: peoplesInSpaceController.state,
-              ),
-            ),
-          ],
-        ),
+      appBar: HeaderPage(
+        context,
+        title: 'People in Space Right Now',
+        description:
+            'The number of people in space at this moment. List of names when known.',
+      ),
+      body: ValueListenableBuilder<PeoplesInSpaceState>(
+        builder: (_, state, __) {
+          return DatatableCustom<PeopleInSpaceEntity>(
+            data: state.peopleInSpaceList,
+            loading: state.isLoading,
+            onRefresh: _getData,
+            itemHeader: [
+              DataColumn(label: Text('Name')),
+              DataColumn(label: Text('Craft'))
+            ],
+            itemRow: (people) => [
+              DataCell(Text(people.name)),
+              DataCell(Text(people.craft), placeholder: true),
+            ],
+          );
+        },
+        valueListenable: peoplesInSpaceController.state,
       ),
     );
   }
