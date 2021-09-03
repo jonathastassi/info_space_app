29bc3f1bfda36ebc751547fbecbd4ca6b268be84
Jonathas Tassi <jonathastassi@hotmail.com>
2021-09-03 16:37:35 -0300

    fix loading view background color

--- lib/app/presentation/widgets/loading_widget.dart
+++ lib/app/presentation/widgets/loading_widget.dart
@@ -1,20 +1,25 @@
 import 'package:flutter/material.dart';
 
 class LoadingWidget extends StatelessWidget {
-  const LoadingWidget({Key? key}) : super(key: key);
+  final String title;
+  const LoadingWidget({Key? key, this.title = 'Please, wait!'})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Center(child: CircularProgressIndicator()),
-        SizedBox(
-          height: 16,
-        ),
-        Center(child: Text('Please, wait!')),
-      ],
+    return Container(
+      color: Colors.white.withOpacity(0.6),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(child: CircularProgressIndicator()),
+          SizedBox(
+            height: 16,
+          ),
+          Center(child: Text(title)),
+        ],
+      ),
     );
   }
 }
