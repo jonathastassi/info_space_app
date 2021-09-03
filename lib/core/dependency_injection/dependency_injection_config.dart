8ecafa69582a94982b34280072398e73946a0cd9
Jonathas Tassi <jonathastassi@hotmail.com>
2021-09-03 14:44:23 -0300

    DI configs for iss location

--- lib/core/dependency_injection/dependency_injection_config.dart
+++ lib/core/dependency_injection/dependency_injection_config.dart
@@ -1,11 +1,16 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:info_space_app/app/data/datasources/i_iss_location_map_remote_datasource.dart';
 import 'package:info_space_app/app/data/datasources/i_people_in_space_local_datasource.dart';
 import 'package:info_space_app/app/data/datasources/i_people_in_space_remote_datasource.dart';
+import 'package:info_space_app/app/data/repositories/iss_location_map_repository.dart';
 import 'package:info_space_app/app/data/repositories/people_in_space_repository.dart';
+import 'package:info_space_app/app/domain/repositories/i_iss_location_map_repository.dart';
 import 'package:info_space_app/app/domain/repositories/i_people_in_space_repository.dart';
+import 'package:info_space_app/app/domain/usecases/get_iss_location_map_usecase.dart';
 import 'package:info_space_app/app/domain/usecases/get_peoples_in_space_usecase.dart';
+import 'package:info_space_app/app/external/datasources/iss_location_map_remote_datasource.dart';
 import 'package:info_space_app/app/external/datasources/people_in_space_remote_datasource.dart';
 import 'package:info_space_app/app/external/http/http_client.dart';
 import 'package:info_space_app/app/external/http/i_http_client.dart';
@@ -13,6 +18,7 @@ import 'package:info_space_app/app/internal/datasources/people_in_space_local_da
 import 'package:info_space_app/app/internal/local_storage/i_local_storage.dart';
 import 'package:info_space_app/app/internal/local_storage/local_storage.dart';
 import 'package:info_space_app/app/presentation/pages/home/home_controller.dart';
+import 'package:info_space_app/app/presentation/pages/iss_location_map/iss_location_map_controller.dart';
 import 'package:info_space_app/app/presentation/pages/peoples_in_space/peoples_in_space_controller.dart';
 import 'package:info_space_app/app/presentation/pages/splash/splash_controller.dart';
 import 'package:info_space_app/core/network_status/i_network_status.dart';
@@ -66,6 +72,12 @@ void configDatasources() {
       httpClient: locator.get<IHttpClient>(),
     ),
   );
+
+  locator.registerLazySingleton<IIssLocationMapRemoteDatasource>(
+    () => IssLocationMapRemoteDatasource(
+      httpClient: locator.get<IHttpClient>(),
+    ),
+  );
 }
 
 void configRepositories() {
@@ -78,12 +90,26 @@ void configRepositories() {
           locator.get<IPeopleInSpaceRemoteDatasource>(),
     ),
   );
+
+  locator.registerLazySingleton<IIssLocationMapRepository>(
+    () => IssLocationMapRepository(
+      networkStatus: locator.get<INetworkStatus>(),
+      issLocationMapRemoteDatasource:
+          locator.get<IIssLocationMapRemoteDatasource>(),
+    ),
+  );
 }
 
 void configUsecases() {
   locator.registerLazySingleton<GetPeoplesInSpaceUsecase>(
     () => GetPeoplesInSpaceUsecase(
       peoplesInSpaceRepository: locator.get<IPeopleInSpaceRepository>(),
+    ),
+  );
+
+  locator.registerLazySingleton<GetIssLocationMapUsecase>(
+    () => GetIssLocationMapUsecase(
+      issLocationMapRepository: locator.get<IIssLocationMapRepository>(),
     ),
   );
 }
@@ -100,6 +126,11 @@ void configControllers() {
   locator.registerLazySingleton<PeoplesInSpaceController>(
     () => PeoplesInSpaceController(
       getPeoplesInSpaceUsecase: locator.get<GetPeoplesInSpaceUsecase>(),
+    ),
+  );
+  locator.registerLazySingleton<IssLocationMapController>(
+    () => IssLocationMapController(
+      getIssLocationMapUsecase: locator.get<GetIssLocationMapUsecase>(),
     ),
   );
 }
